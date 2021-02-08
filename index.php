7e211faee8a30bad6767afd3a16988cae8a0ffc2
Md Masud <masuddlnc@gmail.com>
2021-02-08 12:57:26 +0600

    test

--- index.php
+++ index.php
@@ -29,6 +29,12 @@ body {
 </style>
 <body>
 <h1>How to Get Current Location using Google Map Javascript API</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	<div id="button-layer"><button id="btnAction" onClick="locate()">My Current Location</button></div>
 	<div id="map-layer"></div>
 
@@ -44,7 +50,7 @@ body {
 
 		map = new google.maps.Map(mapLayer, defaultOptions);
 	}
-
+//this is comment for git hub
 	function locate(){
 		document.getElementById("btnAction").disabled = true;
 		document.getElementById("btnAction").innerHTML = "Processing...";
